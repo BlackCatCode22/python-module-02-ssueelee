b18afea82fcde193e3f10fdceea6038896455cee
ssueelee <leessue97@gmail.com>
2025-02-10 05:31:50 +0000

    largestofThree done

---
+++ largestOfThree.py
@@ -0,0 +1,19 @@
+# Get three integers from the user
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+num3 = int(input("Enter the third integer: "))
+
+# Find the largest number using nested if statements
+if num1 >= num2:
+    if num1 >= num3:
+        largest = num1
+    else:
+        largest = num3
+else:
+    if num2 >= num3:
+        largest = num2
+    else:
+        largest = num3
+
+# Output the largest number
+print("The largest number is:", largest)
